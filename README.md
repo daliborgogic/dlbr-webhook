bc9acf0f094dcaea3502a7bc3977ef0c25b19c49
Dalibor Gogic <mail@daliborgogic.com>
2017-07-31 20:00:15 +0200

    inital webhooks

--- README.md
+++ README.md
@@ -8,3 +8,12 @@ EOL
 
 npm run dev
 ```
+
+### Simulating a request using curl
+
+```bash
+# without credentials
+$ curl -X POST --header "X-Contentful-Topic: ContentManagement.Entry.publish" localhost:5000
+# with authentication
+$ curl -X POST -u username:password --header "X-Contentful-Topic: ContentManagement.Entry.publish" localhost:5000
+```
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 const fs = require('fs')
 const express = require('express')
+const webhookServer = require('contentful-webhook-server')({
+  username: 'username',
+  password: 'password'
+})
 const app = express()
-
-
-
-
+const PORT = process.env.PORT || 5000
 const contentful = require('contentful')
 const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE,
@@ -26,10 +27,58 @@ client.sync({
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.json({entries: []})
+  res.send('Express response')
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, (_ =>
+app.use('/', webhookServer.mountAsMiddleware)
+
+const server = app.listen(PORT, (_ =>
   console.log(`*:${PORT}`)
 ))
+
+// Handler for all successful requests
+// Is not emitted when an error occurs
+webhookServer.on('ContentManagement.*', (topic, req) =>
+  // topic is available as string
+  // e.g. ContentManagement.Asset.unpublish
+  console.log('Request came in for: ' + topic)
+)
+
+webhookServer.on('ContentManagement.ContentType.publish', (req =>
+  console.log('A content type was published!')
+))
+
+webhookServer.on('ContentManagement.ContentType.unpublish', (req =>
+  console.log('A content type was unpublished!')
+))
+
+webhookServer.on('ContentManagement.Entry.publish', (req => {
+  console.log('An entry was published!')
+}))
+
+webhookServer.on('ContentManagement.Entry.unpublish', (req => {
+  console.log('An entry was unpublished!')
+}))
+
+webhookServer.on('ContentManagement.Asset.publish', (req =>
+  console.log('An asset was published!')
+))
+
+webhookServer.on('ContentManagement.Asset.unpublish', (req =>
+  console.log('An asset was unpublished!')
+))
+
+// Handle errors
+webhookServer.on('ContentManagement.error', (err, req) =>
+  console.log(err)
+)
+
+function cleanup () {
+  console.log(` Bye .`)
+  process.exit(0)
+}
+
+// If the Node process ends, cleanup existing connections
+process.on('SIGINT', cleanup)
+process.on('SIGTERM', cleanup)
+process.on('SIGHUP', cleanup)
